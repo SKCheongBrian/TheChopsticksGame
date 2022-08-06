5794212176e49e9ac155c460b25a815028ff88ef
ruoyann <ruoyan@u.nus.edu>
2022-08-06 17:23:39 +0800

    Add routing for home, login, signup pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./pages/home/HomePage";
+import PageRoute from "./config/routes"
+import {BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Router>
+        <PageRoute/>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/signup/Signup.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Signup.css";
+
+function SignupPage() {
+  return (
+    <>
+	<h1>signup page</h1>
+    </>
+  );
+}
+
+export default SignupPage;
