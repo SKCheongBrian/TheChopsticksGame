8cf4e1c110bbc48bf880b34558f17e46109d6f78
ruoyann <ruoyan@u.nus.edu>
2022-08-06 18:15:09 +0800

    Add onClick routing to login section's buttons

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,6 +1,46 @@
-import React from "react";
-import "./Button.css";
+// import React from "react";
+// import "./Button.css";
 
-export function Button({ title }: { title: string }) {
-  return <button>{title}</button>;
+// export function Button({ title }: { title: string }) {
+//   return <button>{title}</button>;
+// }
+
+import React from "react";
+import "./Button.css"
+
+interface Props {
+  // border: string;
+  // color: string;
+  children?: React.ReactNode;
+  // height: string;
+  onClick: () => void;
+  // radius: string
+  // width: string;
 }
+
+const Button: React.FC<Props> = ({ 
+    // border,
+    // color,
+    children,
+    // height,
+    onClick, 
+    // radius,
+    // width
+  }) => { 
+  return (
+    <button 
+      onClick={onClick}
+      // style={{
+      //    backgroundColor: color,
+      //    border,
+      //    borderRadius: radius,
+      //    height,
+      //    width
+      // }}
+    >
+    {children}
+    </button>
+  );
+}
+
+export default Button;
