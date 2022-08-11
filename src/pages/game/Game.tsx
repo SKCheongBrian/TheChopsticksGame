d7fbb9471e70ee7b7f03974fd0bd5e4630b378a8
ruoyann <ruoyan@u.nus.edu>
2022-08-11 18:08:33 +0800

    Add button click animation, flex for game screen

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -9,51 +9,50 @@ import Grid from "@material-ui/core/Grid";
 import Button from "../../components/button/Button";
 import AnimatedButton from "../../components/button/AnimatedButton";
 
-const Item = styled(Paper)(({ theme }) => ({
-  //   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  //   ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  //   color: theme.palette.text.secondary,
-}));
+// const Item = styled(Paper)(({ theme }) => ({
+
+//     backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+//     ...theme.typography.body2,
+
+//   // backgroundColor: "#222831", 
+
+//   // padding: theme.spacing(1),
+//   textAlign: "center",
+//   //   color: theme.palette.text.secondary,
+// }));
 
 function GamePage() {
-  const [shake, setShake] = useState(false);
-
-  const animate = () => {
-    setShake(true);
-    setTimeout(() => setShake(false), 200);
-  };
-
-  const L1 = require("../../assets/images/1L.PNG");
   return (
     <>
       <div className="hands">
-        <Grid container spacing={10}>
+        <Grid container spacing={2}>
           <Grid item xs={6}>
+            <div className="handBox">
             <Button
-              onClick={animate}
-              className={shake ? `shake` : undefined}
               children={
                 <img className="flipped" src={OneLeft} alt="OneRight" />
               }
             ></Button>
+            </div>
           </Grid>
           <Grid item xs={6}>
-            <Item>
-              <img className="flipped" src={OneRight} alt="OneRight" />
-            </Item>
-          </Grid>
-          <Grid item xs={12}></Grid>
-          <Grid item xs={6}>
-            <Item>
-              <img src={OneLeft} alt="OneLeft" />
-            </Item>
+            <div className="handBox">
+            <Button
+              children={
+                <img className="flipped" src={OneRight} alt="OneRight" />
+              }
+            ></Button>
+            </div>
           </Grid>
           <Grid item xs={6}>
-            <Item>
-              <img src={OneRight} alt="OneRight" />
-            </Item>
+            <div className="handBox">
+            <Button children={<img src={OneLeft} alt="OneLeft" />}></Button>
+            </div>
+          </Grid>
+          <Grid item xs={6}>
+            <div className="handBox">
+            <Button children={<img src={OneRight} alt="OneRight" />}></Button>
+            </div>
           </Grid>
         </Grid>
       </div>
