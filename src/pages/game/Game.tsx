5befdd43fd648dec857469d1bc459dae4e53b4cf
ruoyann <ruoyan@u.nus.edu>
2022-08-13 23:52:53 +0800

    Add logic for adding hands, cannot click own hand, splitting

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -1,13 +1,6 @@
 import React, { useState } from "react";
 import "./Game.css";
-import OneLeft from "../../assets/images/1L.PNG";
-import OneRight from "../../assets/images/1R.PNG";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@material-ui/core/Grid";
-import Button from "../../components/button/Button";
-import AnimatedButton from "../../components/button/AnimatedButton";
+import Hands from "./Hands"
 
 // const Item = styled(Paper)(({ theme }) => ({
 
@@ -24,38 +17,7 @@ import AnimatedButton from "../../components/button/AnimatedButton";
 function GamePage() {
   return (
     <>
-      <div className="hands">
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-            <div className="handBox">
-            <Button
-              children={
-                <img className="flipped" src={OneLeft} alt="OneRight" />
-              }
-            ></Button>
-            </div>
-          </Grid>
-          <Grid item xs={6}>
-            <div className="handBox">
-            <Button
-              children={
-                <img className="flipped" src={OneRight} alt="OneRight" />
-              }
-            ></Button>
-            </div>
-          </Grid>
-          <Grid item xs={6}>
-            <div className="handBox">
-            <Button children={<img src={OneLeft} alt="OneLeft" />}></Button>
-            </div>
-          </Grid>
-          <Grid item xs={6}>
-            <div className="handBox">
-            <Button children={<img src={OneRight} alt="OneRight" />}></Button>
-            </div>
-          </Grid>
-        </Grid>
-      </div>
+    <Hands/>
     </>
   );
 }
