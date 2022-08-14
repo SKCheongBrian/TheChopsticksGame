823b2b9fe542aff57b1406678967e5d08fd6f618
skcheongbrian <skcheongbrian@gmail.com>
2022-08-14 10:56:47 +0800

    Remove unnecessary imports on the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./pages/home/HomePage";
-import PageRoute from "./config/routes"
-import {BrowserRouter as Router} from 'react-router-dom'
+import PageRoute from "./config/routes";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Router>
-        <PageRoute/>
+        <PageRoute />
       </Router>
     </div>
   );
