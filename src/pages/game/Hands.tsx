0b0ab3dcdb75e60b455614861a3e04c0ef21a038
skcheongbrian <skcheongbrian@gmail.com>
2022-08-14 15:30:24 +0800

    Refactored some code

--- src/pages/game/Hands.tsx
+++ src/pages/game/Hands.tsx
@@ -10,17 +10,8 @@ import ThreeLeft from "../../assets/images/3L.PNG";
 import ThreeRight from "../../assets/images/3R.PNG";
 import FourLeft from "../../assets/images/4L.PNG";
 import FourRight from "../../assets/images/4R.PNG";
-import FiveLeft from "../../assets/images/5L.PNG";
-import FiveRight from "../../assets/images/5R.PNG";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Grid from "@material-ui/core/Grid";
 import Button from "../../components/button/Button";
-import AnimatedButton from "../../components/button/AnimatedButton";
-import track, { useTracking } from "react-tracking";
-import { Alert } from "react-alert";
-import { read } from "fs";
 
 function Hands() {
   const [oppL, setOppL] = useState(1);
@@ -29,44 +20,41 @@ function Hands() {
   const [selfR, setSelfR] = useState(1);
   const [showSplitChoice, setShowSplitChoice] = useState(false);
 
-  let numClicks = 0;
+  let numClicks: number = 0;
   let newNumber: number = 0;
-  let prevHand;
+  let prevHand: any;
 
   function handleClick(num: number, side: string, opp: boolean) {
-    if (num > 4) {
+    if (num === 0) {
       alert("cannot use this hand");
       return;
     }
 
     numClicks += 1;
     // a new pair of clicks
-    if (numClicks == 1) {
+    if (numClicks === 1) {
       newNumber = num;
-      prevHand = {num, side, opp};
-    } else if (numClicks == 3) {
-      newNumber = num;
-      prevHand = {num, side, opp};
-      numClicks = 1;
-    } else if (numClicks == 2) {
+      prevHand = { num, side, opp };
+    } else if (numClicks === 2) {
+      numClicks = 0;
       console.log("done with one pair");
-      if (JSON.stringify({num, side, opp}) == JSON.stringify(prevHand)) {
+      if (JSON.stringify({ num, side, opp }) === JSON.stringify(prevHand)) {
         prevHand = undefined;
-        alert ("cannot add to the same hand");
+        alert("cannot add to the same hand");
         return;
       }
       // change this hand's number
       if (opp) {
-        if (side == "L") {
-          setOppL(newNumber + num);
+        if (side === "L") {
+          setOppL(newNumber + num > 4 ? 0 : newNumber + num);
         } else {
-          setOppR(newNumber + num);
+          setOppR(newNumber + num > 4 ? 0 : newNumber + num);
         }
       } else {
-        if (side == "L") {
-          setSelfL(newNumber + num);
+        if (side === "L") {
+          setSelfL(newNumber + num > 4 ? 0 : newNumber + num);
         } else {
-          setSelfR(newNumber + num);
+          setSelfR(newNumber + num > 4 ? 0 : newNumber + num);
         }
       }
     }
@@ -77,14 +65,6 @@ function Hands() {
   function returnHandImage(num: number, side: string, opp: boolean) {
     if (side === "L") {
       switch (num) {
-        // case 0:
-        //   return (
-        //     <img
-        //       className={opp ? "flipped" : ""}
-        //       src={ZeroLeft}
-        //       alt="ZeroLeft"
-        //     />
-        //   );
         case 1:
           return (
             <img className={opp ? "flipped" : ""} src={OneLeft} alt="OneLeft" />
@@ -109,14 +89,6 @@ function Hands() {
               alt="FourLeft"
             />
           );
-        // case 5:
-        //   return (
-        //     <img
-        //       className={opp ? "flipped" : ""}
-        //       src={FiveLeft}
-        //       alt="FiveLeft"
-        //     />
-        //   );
         default:
           return (
             <img
@@ -128,14 +100,6 @@ function Hands() {
       }
     } else {
       switch (num) {
-        // case 0:
-        //   return (
-        //     <img
-        //       className={opp ? "flipped" : ""}
-        //       src={ZeroRight}
-        //       alt="ZeroRight"
-        //     />
-        //   );
         case 1:
           return (
             <img
@@ -168,14 +132,6 @@ function Hands() {
               alt="FourRight"
             />
           );
-        // case 5:
-        //   return (
-        //     <img
-        //       className={opp ? "flipped" : ""}
-        //       src={FiveRight}
-        //       alt="FiveRight"
-        //     />
-        //   );
         default:
           return (
             <img
@@ -193,18 +149,18 @@ function Hands() {
     let num1 = selfL;
     let num2 = selfR;
     // 2 0 -> 1 1
-    if ((num1 == 2 && num2 > 4) || (num1 > 4 && num2 == 2)) {
+    if ((num1 === 2 && num2 === 0) || (num1 === 0 && num2 === 2)) {
       setSelfL(1);
       setSelfR(1);
     }
     // 3 0 -> 1 2
-    else if ((num1 == 3 && num2 > 4) || (num1 > 4 && num2 == 3)) {
+    else if ((num1 === 3 && num2 === 0) || (num1 === 0 && num2 === 3)) {
       setSelfL(1);
       setSelfR(2);
     }
     // 4 0 -> 2 2
     // 4 0 -> 1 3
-    else if ((num1 == 4 && num2 > 4) || (num1 > 4 && num2 == 4)) {
+    else if ((num1 == 4 && num2 === 0) || (num1 === 0 && num2 == 4)) {
       setShowSplitChoice(true);
       // setSelfL(2);
       // setSelfR(2);
@@ -214,7 +170,7 @@ function Hands() {
       setSelfL(2);
       setSelfR(2);
     }
-    // 2 2  -> 1 3 
+    // 2 2  -> 1 3
     else if (num1 == 2 && num2 == 2) {
       setSelfL(1);
       setSelfR(3);
@@ -224,7 +180,7 @@ function Hands() {
       setSelfL(2);
       setSelfR(3);
     }
-    // 2 3 -> 1 4 
+    // 2 3 -> 1 4
     else if ((num1 == 2 && num2 == 3) || (num1 == 3 && num2 == 2)) {
       setSelfL(1);
       setSelfR(4);
@@ -234,13 +190,12 @@ function Hands() {
       setSelfL(3);
       setSelfR(3);
     }
-    // 3 3 -> 2 4 
+    // 3 3 -> 2 4
     else if (num1 == 3 && num2 == 3) {
       setSelfL(2);
       setSelfR(4);
-    }
-    else {
-      alert("cannot do split")
+    } else {
+      alert("cannot do split");
     }
   }
 
@@ -275,19 +230,26 @@ function Hands() {
           </div>
         </Grid>
         <Grid item xs={12}>
-          {!showSplitChoice ?
+          {!showSplitChoice ? (
             <Button
               className="splitButton"
               children="SPLIT"
               onClick={() => handleSplit()}
             />
-          : 
-          <div className="splitChoices">
-          <Button className ="splitChoices" children="2:2" 
-          onClick={() => split22()}/>
-          <Button className ="splitChoices" children="1:3" onClick={() => split13()}/> 
-          </div>
-          }
+          ) : (
+            <div className="splitChoices">
+              <Button
+                className="splitChoices"
+                children="2:2"
+                onClick={() => split22()}
+              />
+              <Button
+                className="splitChoices"
+                children="1:3"
+                onClick={() => split13()}
+              />
+            </div>
+          )}
         </Grid>
         <Grid item xs={6}>
           <div className="handBox">
